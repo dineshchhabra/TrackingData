f38ceac6c83bdc1b370aade177ff07fcad979e3c
dineshchhabra <test@L-MAA-25004134.paypalcorp.com>
2015-07-27 18:38:55 +0530

    updating signature of TrackingRecord

--- src/TrackingRecord.java
+++ src/TrackingRecord.java
@@ -1,7 +1,32 @@
 import java.io.*;
 import java.util.*;
 
+class TrackRecord {
+	Range startEndTansctionCode;
+	int transferCode;
+	int statusCode;
+}
+
 public class TrackingRecord {
+	
+	ArrayList<TrackRecord> trackRecords;
+	
+	TrackingRecord() {
+		trackRecords = new ArrayList<TrackRecord>();
+	}
+	
+	public displayTrackRecords() {
+		
+	}
+	
+	public updateTrackRecord() {
+		
+	}
+	
+	private mergeRecords() {
+		
+	}
+	
 	
 	public static void main(String[] args) throws IOException
 	{
@@ -18,6 +43,8 @@ public class TrackingRecord {
 		}
 		
 	}
+	
+	
 }
 	
 
